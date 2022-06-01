0c19f311e76e20374a86859df0fac3e8773833c5
TheCaister <edisoncai2001@gmail.com>
2022-06-01 11:41:17 +0200

    Fixed bug with underscores

--- src/com/thecaister/imageprocessing/ImageProcessing.java
+++ src/com/thecaister/imageprocessing/ImageProcessing.java
@@ -1,55 +1,75 @@
 package com.thecaister.imageprocessing;
 
-import java.io.*;
-import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ImageProcessing {
-    public static void GreyScale(String filename){
+    public static boolean checkStringIsInt(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException ignored) {
+        }
+        return false;
+    }
+
+    public static String combineStringArray(String[] array) {
+        StringBuilder output = new StringBuilder();
+
+        for (String s : array) {
+            output.append(s);
+        }
+
+        return output.toString();
+    }
+
+    public static File findFile(String filename) {
+        File file;
+        file = new File(filename);
+
+        if (!file.exists()) {
+            file = new File("images/" + filename + ".jpg");
+        }
+
+        if (!file.exists()) {
+            file = new File("images/" + filename + ".png");
+        }
+
+        if (!file.exists()) {
+            file = new File("images/NoImagePlaceholder.png");
+        }
+
+        return file;
+    }
+
+    public static void GreyScale(String filename) {
         BufferedImage image = null;
         File file = null;
 
-        try
-        {
-            file = new File(filename);
-
-            if(!file.exists()){
-                file = new File("images/" + filename + ".jpg");
-            }
-
-            if(!file.exists()){
-                file = new File("images/" + filename + ".png");
-            }
-
-            if(!file.exists()){
-                file = new File("images/Among_Us_cover_art.jpg");
-            }
-
+        try {
+            file = findFile(filename);
             image = ImageIO.read(file);
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(e);
         }
 
         int width = image.getWidth();
         int height = image.getHeight();
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                int p = image.getRGB(x,y);
-                int a = (p>>24) & 0xff;
-                int r = (p>>16) & 0xff;
-                int g = (p>>8) & 0xff;
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = image.getRGB(x, y);
+                int a = (p >> 24) & 0xff;
+                int r = (p >> 16) & 0xff;
+                int g = (p >> 8) & 0xff;
                 int b = p & 0xff;
-                int avg = (r+g+b)/3;
-                p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+                int avg = (r + g + b) / 3;
+                p = (a << 24) | (avg << 16) | (avg << 8) | avg;
                 image.setRGB(x, y, p);
             }
         }
-        try
-        {
+        try {
             String newPathName = file.getName();
 
             // e.g. house_0.png splits into [0] = "house_0" and [1] = "png"
@@ -58,36 +78,44 @@ public class ImageProcessing {
             // e.g. house_0 splits into [0] = "house" and [1] = "0"
             String[] splitFromVersion = splitFromExtension[0].split("_");
 
+            int splitFromExtensionLastIndex = splitFromExtension.length - 1;
+            int splitFromVersionLastIndex = splitFromVersion.length - 1;
+
+
             int versionNumber;
 
-            if(splitFromVersion.length == 1){
+            if (checkStringIsInt(splitFromVersion[splitFromVersionLastIndex])) {
+                versionNumber = Integer.parseInt(splitFromVersion[splitFromVersionLastIndex]);
+
+            } else {
                 versionNumber = 0;
-            }
-            else{
-                versionNumber = Integer.parseInt(splitFromVersion[1]);
+
             }
 
             System.out.println(newPathName);
+            StringBuilder fileNameWithoutVersion = new StringBuilder();
+
+            for (int i = 0; i < splitFromVersionLastIndex; i++) {
+                fileNameWithoutVersion.append(splitFromVersion[i]);
+                fileNameWithoutVersion.append("_");
+            }
+            fileNameWithoutVersion.append(splitFromVersion[splitFromVersionLastIndex]);
 
             // Next, we check to see which version this next image will be.
             // If there is a house_0.png already, we should make an image called house_1.png
-            file = new File("images/" + splitFromVersion[0] + "_" + versionNumber + "." + splitFromExtension[1]);
+            file = new File("images/" + fileNameWithoutVersion + "_" + versionNumber + "." + splitFromExtension[splitFromExtensionLastIndex]);
 
-            while(file.exists()){
+
+            while (file.exists()) {
                 versionNumber++;
-                file = new File("images/" + splitFromVersion[0] + "_" + versionNumber + "." + splitFromExtension[1]);
+                file = new File("images/" + fileNameWithoutVersion + "_" + versionNumber + "." + splitFromExtension[splitFromExtensionLastIndex]);
+
                 System.out.println();
             }
 
-
-            //System.out.println(parts[0] + parts[1]);
-            //file = new File("images/amongus.png");
-            //file = new File("images/amongus.png");
             ImageIO.write(image, "png", file);
             System.out.println("Successfully converted a colored image into a grayscale image");
-        }
-        catch(IOException e)
-        {
+        } catch (IOException e) {
             System.out.println(e);
         }
     }
